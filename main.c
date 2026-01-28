b913925220e06d02e0223a19d3944415ba25493e
Mautar55 <cuanticidadcosmica@live.com>
2026-01-28 10:36:34 -0300

    some testing

--- main.c
+++ main.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "raylib.h"
+#include "wutils.h"
 
 int main(void) {
     InitWindow(800, 600, "Hello Raylib");
     SetTargetFPS(60);
 
+    CircleList circles = {0};
+    srand(time(NULL));
+    for (int i = 0; i < 30; i++) {
+        CircleData c = { rand() % 800, rand() % 600 };
+        da_append(circles, c);
+    }
+
     while (!WindowShouldClose()) {
+        if (IsKeyPressed(KEY_K)) {
+            CircleData c = { rand() % GetScreenWidth(), rand() % GetScreenHeight() };
+            da_append(circles, c);
+        }
+        if (IsKeyPressed(KEY_L)) {
+            if (circles.size > 0) circles.size--;
+        }
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello World from Raylib + CLion!", 190, 200, 20, BLACK);
+        
+        for (size_t i = 0; i < circles.size; i++) {
+            DrawCircle(circles.items[i].position_x, circles.items[i].position_y, 5, DARKGREEN);
+        }
+        
         EndDrawing();
     }
 
---
+++ wutils.h
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include <stdlib.h>
+
+typedef struct {
+    int position_x;
+    int position_y;
+} CircleData;
+
+typedef struct {
+    CircleData *items;
+    size_t size;
+    size_t capacity;
+} CircleList;
+
+#define da_append(xs, x)\
+    do {\
+        if ((xs).size >= (xs).capacity) {\
+            if ((xs).capacity == 0) (xs).capacity = 256;\
+            else (xs).capacity *= 2;\
+            (xs).items = realloc((xs).items, (xs).capacity * sizeof(*(xs).items));\
+        }\
+        (xs).items[(xs).size++] = x;\
+    } while(0)

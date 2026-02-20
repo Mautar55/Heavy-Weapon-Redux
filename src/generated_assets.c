242e0381db6214cfeb3420d8a7a5b4e183de38a0
Mauricio Tarno <55maurit@gmail.com>
2026-02-20 00:09:58 -0300

    deleted assets experimental assets and added new ones

--- src/generated_assets.c
+++ src/generated_assets.c
@@ -1,20 +1,23 @@
 #include "generated_assets.h"
 
 const char * ass_bombfrag_png = "../assets/bombfrag.png";
-const char * ass_characters_character_female_d_glb = "../assets/characters/character-female-d.glb";
-const char * ass_characters_character_female_d_png = "../assets/characters/character-female-d.png";
-const char * ass_characters_character_female_e_glb = "../assets/characters/character-female-e.glb";
-const char * ass_characters_character_female_e_png = "../assets/characters/character-female-e.png";
-const char * ass_characters_character_male_d_glb = "../assets/characters/character-male-d.glb";
-const char * ass_characters_character_male_d_png = "../assets/characters/character-male-d.png";
-const char * ass_characters_character_male_e_glb = "../assets/characters/character-male-e.glb";
-const char * ass_characters_character_male_e_png = "../assets/characters/character-male-e.png";
-const char * ass_characters_Textures_colormap_png = "../assets/characters/Textures/colormap.png";
 const char * ass_fonts_FiraCode_Regular_ttf = "../assets/fonts/FiraCode-Regular.ttf";
 const char * ass_fonts_Georama_SemiCondensed_Bold_ttf = "../assets/fonts/Georama_SemiCondensed-Bold.ttf";
 const char * ass_jetfighter_png = "../assets/jetfighter.png";
 const char * ass_mafalda_128_png = "../assets/mafalda_128.png";
 const char * ass_projectiles_bombfrag_png = "../assets/projectiles/bombfrag.png";
+const char * ass_projectiles_dumbbomb_png = "../assets/projectiles/dumbbomb.png";
+const char * ass_projectiles_fatboy_png = "../assets/projectiles/fatboy.png";
+const char * ass_projectiles_fragbomb_png = "../assets/projectiles/fragbomb.png";
+const char * ass_projectiles_headmissile_png = "../assets/projectiles/headmissile.png";
+const char * ass_projectiles_hellfire_png = "../assets/projectiles/hellfire.png";
+const char * ass_projectiles_ironbomb_png = "../assets/projectiles/ironbomb.png";
+const char * ass_projectiles_lasers_png = "../assets/projectiles/lasers.png";
+const char * ass_projectiles_lgb_png = "../assets/projectiles/lgb.png";
+const char * ass_projectiles_missile_png = "../assets/projectiles/missile.png";
+const char * ass_projectiles_REFERENCE_SCREENSHOT_png = "../assets/projectiles/REFERENCE SCREENSHOT.png";
+const char * ass_projectiles_rocket_png = "../assets/projectiles/rocket.png";
+const char * ass_projectiles_rpg_png = "../assets/projectiles/rpg.png";
 const char * ass_projectiles_tank_bullet_1_png = "../assets/projectiles/tank_bullet_1.png";
 const char * ass_projectiles_tank_bullet_2_png = "../assets/projectiles/tank_bullet_2.png";
 const char * ass_projectiles_tank_bullet_3_png = "../assets/projectiles/tank_bullet_3.png";
--- src/generated_assets.h
+++ src/generated_assets.h
@@ -1,20 +1,23 @@
 #pragma once
 
 extern const char * ass_bombfrag_png;
-extern const char * ass_characters_character_female_d_glb;
-extern const char * ass_characters_character_female_d_png;
-extern const char * ass_characters_character_female_e_glb;
-extern const char * ass_characters_character_female_e_png;
-extern const char * ass_characters_character_male_d_glb;
-extern const char * ass_characters_character_male_d_png;
-extern const char * ass_characters_character_male_e_glb;
-extern const char * ass_characters_character_male_e_png;
-extern const char * ass_characters_Textures_colormap_png;
 extern const char * ass_fonts_FiraCode_Regular_ttf;
 extern const char * ass_fonts_Georama_SemiCondensed_Bold_ttf;
 extern const char * ass_jetfighter_png;
 extern const char * ass_mafalda_128_png;
 extern const char * ass_projectiles_bombfrag_png;
+extern const char * ass_projectiles_dumbbomb_png;
+extern const char * ass_projectiles_fatboy_png;
+extern const char * ass_projectiles_fragbomb_png;
+extern const char * ass_projectiles_headmissile_png;
+extern const char * ass_projectiles_hellfire_png;
+extern const char * ass_projectiles_ironbomb_png;
+extern const char * ass_projectiles_lasers_png;
+extern const char * ass_projectiles_lgb_png;
+extern const char * ass_projectiles_missile_png;
+extern const char * ass_projectiles_REFERENCE_SCREENSHOT_png;
+extern const char * ass_projectiles_rocket_png;
+extern const char * ass_projectiles_rpg_png;
 extern const char * ass_projectiles_tank_bullet_1_png;
 extern const char * ass_projectiles_tank_bullet_2_png;
 extern const char * ass_projectiles_tank_bullet_3_png;
